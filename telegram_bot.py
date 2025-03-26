ea8953f581a3b590b5d78993ccd9416634000b60
Jana <yanix2x2@gmail.com>
2025-03-26 15:41:26 +0300

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,11 +1,25 @@
+import os
+import random
+
 from environs import env
 import telegram
 
 
-def get_message(bot, chat_id):
+def send_message(bot, chat_id):
     bot.send_message(
         chat_id=chat_id,
         text="I'm sorry Dave I'm afraid I can't do that."
+    )
+
+
+def send_picture(bot, chat_id):
+    files = os.listdir('images')
+    file = random.choice(files)
+    path = f'images/{file}'
+
+    bot.send_photo(
+        chat_id=chat_id,
+        photo=open(path, 'rb')
     )
 
 
@@ -15,7 +29,8 @@ def main():
     chat_id = env('CHAT_ID')
     bot = telegram.Bot(token=tg_token)
 
-    get_message(bot, chat_id)
+    # send_message(bot, chat_id)
+    send_picture(bot, chat_id)
 
 
 if __name__ == '__main__':
