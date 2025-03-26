cc461780436a194611eb61ca70bcf124ce31e80e
Jana <yanix2x2@gmail.com>
2025-03-26 18:11:28 +0300

    Delete send_message() and add time.sleep()

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,25 +1,22 @@
 import os
 import random
+import time
+import argparse
 
 from environs import env
 import telegram
 
 
-def send_message(bot, chat_id):
-    bot.send_message(
-        chat_id=chat_id,
-        text="I'm sorry Dave I'm afraid I can't do that."
-    )
-
-
 def send_picture(bot, chat_id):
-    files = os.listdir('images')
-    file = random.choice(files)
-    path = f'images/{file}'
+    path = []
+    for address, dirs, files in os.walk('images'):
+        for file in files:
+            path.append(os.path.join(address, file))
 
+    random.shuffle(path)
     bot.send_photo(
         chat_id=chat_id,
-        photo=open(path, 'rb')
+        photo=open(random.choice(path), 'rb')
     )
 
 
@@ -29,8 +26,22 @@ def main():
     chat_id = env('CHAT_ID')
     bot = telegram.Bot(token=tg_token)
 
-    # send_message(bot, chat_id)
-    send_picture(bot, chat_id)
+    parser = argparse.ArgumentParser(
+            description='Время автоматической публикации'
+        )
+    parser.add_argument(
+        '--time', 
+        '-t',
+        type=int,
+        help='Время в секундах',
+        default='4 * 3600'
+    )
+    args = parser.parse_args()
+    print(args.time)
+
+    while True:
+        time.sleep(args.time)
+        send_picture(bot, chat_id)
 
 
 if __name__ == '__main__':
