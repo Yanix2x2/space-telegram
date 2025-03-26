bad81922fbc4ff8cafc1032477ffa757eff1134f
Jana <yanix2x2@gmail.com>
2025-03-26 14:59:10 +0300

    add fetch_nasa_apod.py

---
+++ fetch_nasa_apod.py
@@ -0,0 +1,41 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+from helper import get_file_extension
+
+
+def get_links_nasa_images(api_key):
+    url = 'https://api.nasa.gov/planetary/apod'
+    params = {'api_key': api_key, 'count': 30}
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    links_nasa = []
+    for link in response.json():
+        links_nasa.append(link['url'])
+    return links_nasa
+
+
+def fetch_nasa_apod(links_nasa, directory):
+    for i, link in enumerate(links_nasa):
+        response = requests.get(link)
+        response.raise_for_status()
+        file_extension = get_file_extension(link)
+        image_name = f'nasa_apod_{i}{file_extension}'
+        path = os.path.join(directory, image_name)
+        with open(path, 'wb') as image:
+            image.write(response.content)
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ["NASA_API_KEY"]
+    directory = 'images' 
+    os.makedirs(directory, exist_ok=True)
+
+    links_nasa = get_links_nasa_images(api_key)
+    fetch_nasa_apod(links_nasa, directory)
+
+
+if __name__ == '__main__':
+    main()
