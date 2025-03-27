1d3c0d71c0f089892d4a1664b8d8ff8d4fc50964
Jana <yanix2x2@gmail.com>
2025-03-27 18:54:54 +0300

    Changed name, add save_pictures(), add argparse

--- fetch_nasa_apod.py
+++ fetch_nasa_apod.py
@@ -1,42 +1,41 @@
 import os
 import requests
 from dotenv import load_dotenv
+import argparse
 
-from helper import get_file_extension
+from helper import get_file_extension, save_pictures
 
 
-def get_links_nasa_images(api_key):
+def get_nasa_links(api_key, count):
     url = 'https://api.nasa.gov/planetary/apod'
-    params = {'api_key': api_key, 'count': 50}
+    params = {'api_key': api_key, 'count': count}
     response = requests.get(url, params=params)
     response.raise_for_status()
-    links_nasa = []
-    for link in response.json():
-        links_nasa.append(link.get('url'))
-    return links_nasa
+    nasa_links = [link.get('url') for link in response.json()]
+    return nasa_links
 
 
-def fetch_nasa_apod(links_nasa, directory):
-    for i, link in enumerate(links_nasa):
+def fetch_nasa_apod(nasa_links, directory):
+    for i, link in enumerate(nasa_links):
         if not link:
             continue
-        response = requests.get(link)
-        response.raise_for_status()
         file_extension = get_file_extension(link)
         image_name = f'nasa_apod_{i}{file_extension}'
-        path = os.path.join(directory, image_name)
-        with open(path, 'wb') as image:
-            image.write(response.content)
+        save_pictures(link, directory, image_name)
 
 
 def main():
     load_dotenv()
     api_key = os.environ["NASA_API_KEY"]
-    directory = 'images/nasa' 
-    os.makedirs(directory, exist_ok=True)
+    
+    parser = argparse.ArgumentParser(description='Загрузка фотографий с Nasa')
+    parser.add_argument('--directory', '-d', help='Директория для фото', default='images/nasa')
+    parser.add_argument('--count', '-c', help='Сколько скачать фото', default='30')
+    args = parser.parse_args()
 
-    links_nasa = get_links_nasa_images(api_key)
-    fetch_nasa_apod(links_nasa, directory)
+    os.makedirs(args.directory, exist_ok=True)
+    nasa_links = get_nasa_links(api_key, args.count)
+    fetch_nasa_apod(nasa_links, args.directory)
 
 
 if __name__ == '__main__':
--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -3,45 +3,37 @@ import requests
 import argparse
 from urllib.parse import urljoin
 
+from helper import save_pictures
+
 BASE_URL = 'https://api.spacexdata.com/v5/launches/'
 
 
-def get_links_spacex_images():
-    parser = argparse.ArgumentParser(
-        description='Загрузка фотографий с указанным id'
-    )
-    parser.add_argument(
-        '--launch_id',
-        '-l',
-        help='ID запуска',
-        default='latest'
-    )
-    args = parser.parse_args()
-    url = urljoin(BASE_URL, args.launch_id)
+def get_spacex_links(launch_id):
+    url = urljoin(BASE_URL, launch_id)
     response = requests.get(url)
     response.raise_for_status()
-    links_spacex = response.json()['links']['flickr']['original']
-    return links_spacex
+    spacex_links = response.json()['links']['flickr']['original']
+    return spacex_links
 
 
-def fetch_spacex_last_launch(links_spacex, directory):
-    for i, link in enumerate(links_spacex):
-        response = requests.get(link)
-        response.raise_for_status()
-
+def fetch_spacex_last_launch(spacex_links, directory):
+    for i, link in enumerate(spacex_links):
         image_name = f'spacex_{i}.jpg'
-        path = os.path.join(directory, image_name)
-        with open(path, 'wb') as image:
-            image.write(response.content)
+        save_pictures(link, directory, image_name)
 
 
 def main():
-    directory = 'images/spacex'
-    
-    os.makedirs(directory, exist_ok=True)
+    parser = argparse.ArgumentParser(
+        description='Загрузка фотографий со SpaceX с указанным id запуска'
+    )
+    parser.add_argument('--launch_id', '-l', help='ID запуска', default='latest')
+    parser.add_argument('--directory', '-d', help='Директория для фото', default='images/spacex')
+    args = parser.parse_args()
 
-    links_spacex = get_links_spacex_images()
-    fetch_spacex_last_launch(links_spacex, directory)
+    os.makedirs(args.directory, exist_ok=True)
+
+    spacex_links = get_spacex_links(args.launch_id)
+    fetch_spacex_last_launch(spacex_links, args.directory)
 
 
 if __name__ == '__main__':
