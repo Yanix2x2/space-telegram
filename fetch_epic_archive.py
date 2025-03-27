534b5b9f6d0f596ab73163428b41ce63c51e48a3
Jana <yanix2x2@gmail.com>
2025-03-27 18:54:29 +0300

    Changed name, add save_pictures(), add argparse

--- fetch_epic_archive.py
+++ fetch_epic_archive.py
@@ -3,17 +3,20 @@ import requests
 from dotenv import load_dotenv
 from datetime import datetime
 from urllib.parse import urljoin
+import argparse
+
+from helper import save_pictures
 
 
 BASE_URL = 'https://api.nasa.gov/EPIC/api/natural'
 
 
-def get_links_epic_images(api_key):
+def get_epic_links(api_key):
     params = {'api_key': api_key}
     response = requests.get(BASE_URL, params=params)
     response.raise_for_status()
 
-    links_epic = []
+    epic_links = []
     archive_url = urljoin(BASE_URL, '../archive/natural/')
     for response in response.json():
         date = (
@@ -23,29 +26,28 @@ def get_links_epic_images(api_key):
         )
         filename = f'{response['image']}.png'
         link = urljoin(archive_url, f'{date}/png/{filename}')
-        links_epic.append(link)
-    return links_epic
+        epic_links.append(link)
+    return epic_links
 
 
-def fetch_epic_archive(links_epic, directory, api_key):
+def fetch_epic_archive(epic_links, directory, api_key):
     params = {'api_key': api_key}
-    for i, link in enumerate(links_epic):
-        response = requests.get(link, params=params)
-        response.raise_for_status()
+    for i, link in enumerate(epic_links):
         image_name = f'epic_archive_{i}.png'
-        path = os.path.join(directory, image_name)
-        with open(path, 'wb') as image:
-            image.write(response.content)
+        save_pictures(link, directory, image_name, params=params)
 
 
 def main():
     load_dotenv()
     api_key = os.environ["NASA_API_KEY"]
-    directory = 'images/epic'
-    os.makedirs(directory, exist_ok=True)
 
-    links_epic = get_links_epic_images(api_key)
-    fetch_epic_archive(links_epic, directory, api_key)
+    parser = argparse.ArgumentParser(description='Загрузка фотографий с Nasa')
+    parser.add_argument('--directory', '-d', help='Директория для фото', default='images/epic')
+    args = parser.parse_args()
+
+    os.makedirs(args.directory, exist_ok=True)
+    epic_links = get_epic_links(api_key)
+    fetch_epic_archive(epic_links, args.directory, api_key)
 
 
 if __name__ == '__main__':
