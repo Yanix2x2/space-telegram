786c7e58055f3487d12903110bd4288db8842547
Jana <yanix2x2@gmail.com>
2025-03-27 18:55:07 +0300

    add 2 func

--- helper.py
+++ helper.py
@@ -1,6 +1,8 @@
 import os
 from urllib.parse import urlparse
 
+import requests
+
 
 def get_file_extension(link):
     parsed = urlparse(link)
@@ -9,3 +11,19 @@ def get_file_extension(link):
     extension = os.path.splitext(filename)[1]
 
     return extension.lower()
+
+
+def save_pictures(link, directory, image_name, params=None):
+    response = requests.get(link, params=params)
+    response.raise_for_status()
+    path = os.path.join(directory, image_name)
+    with open(path, 'wb') as image:
+        image.write(response.content)
+
+
+def directory_walk(directory):
+    path = []
+    for address, dirs, files in os.walk(directory):
+        for file in files:
+            path.append(os.path.join(address, file))
+    return path
